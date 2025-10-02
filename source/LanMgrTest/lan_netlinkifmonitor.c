aa386b7e8362bd079ab78563b2f9d72dde5e955a
rirfha948 <rasina_irfhan@comcast.com>
2025-10-02 16:47:07 +0000

    RDKB-1234000: LAN Manager - Netlink code to monitor interface creation and deletion
    Reason for change: Lan manager will add to a list or array on which all interface net link code should monitor
    Test Procedure:
      - TBD
    Risks: None
    Priority: P3
    
    Signed-off-by: rirfha948 <rasina_irfhan@comcast.com>

--- source/LanMgrTest/lan_netlinkifmonitor.c
+++ source/LanMgrTest/lan_netlinkifmonitor.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
+#include <string.h>
 #include <errno.h>
 #include <sys/socket.h>
 #include <linux/netlink.h>
 #include <linux/rtnetlink.h>
 #include <net/if.h>
 
-// Buffer size for netlink messages
 #define NL_BUFSIZE 8192
+#define MAX_INTERFACES 32   // Max interfaces we allow as arguments
 
-// Logging macro
-#define monitor_log(fmt, ...)   fprintf(stderr, "[DEBUG] " fmt "\n", ##__VA_ARGS__)
-
+// Store interface list
+static const char *monitored_ifaces[MAX_INTERFACES];
+static int monitored_count = 0;
 
 // Helper: parse attributes
 static void parse_attributes(struct rtattr *tb[], int max, struct rtattr *rta, int len) {
@@ -24,13 +24,28 @@ static void parse_attributes(struct rtattr *tb[], int max, struct rtattr *rta, i
     }
 }
 
+// ✅ Callback function when interface comes UP
+void on_interface_up(const char *ifname, int ifindex) {
+    printf(">>> CALLBACK: Interface %s (index %d) is UP <<<\n", ifname, ifindex);
+}
+
+// Function to check if interface is in monitored list
+static int is_monitored(const char *ifname) {
+    for (int i = 0; i < monitored_count; i++) {
+        if (strcmp(monitored_ifaces[i], ifname) == 0) {
+            return 1;
+        }
+    }
+    return 0;
+}
+
 // Function to handle link events
 void handle_link_event(struct nlmsghdr *nlh) {
     struct ifinfomsg *ifi = NLMSG_DATA(nlh);
     int len = nlh->nlmsg_len - NLMSG_LENGTH(sizeof(*ifi));
+
     struct rtattr *tb[IFLA_MAX + 1];
     memset(tb, 0, sizeof(tb));
-
     parse_attributes(tb, IFLA_MAX, IFLA_RTA(ifi), len);
 
     const char *ifname = NULL;
@@ -38,16 +53,33 @@ void handle_link_event(struct nlmsghdr *nlh) {
         ifname = (char *)RTA_DATA(tb[IFLA_IFNAME]);
     }
 
+    if (!ifname || !is_monitored(ifname)) {
+        return; // Ignore interfaces we don't care about
+    }
+
     if (nlh->nlmsg_type == RTM_NEWLINK) {
-        printf("Interface added/updated: %s (index %d)\n", 
-               ifname ? ifname : "unknown", ifi->ifi_index);
-    } else if (nlh->nlmsg_type == RTM_DELLINK) {
-        printf("Interface deleted: %s (index %d)\n", 
-               ifname ? ifname : "unknown", ifi->ifi_index);
+        if (ifi->ifi_flags & IFF_UP) {
+            on_interface_up(ifname, ifi->ifi_index);
+        }
     }
 }
 
-int main() {
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        fprintf(stderr, "Usage: %s <iface1> [iface2] ... [ifaceN]\n", argv[0]);
+        return 1;
+    }
+
+    // Save monitored interfaces
+    monitored_count = argc - 1;
+    if (monitored_count > MAX_INTERFACES) {
+        fprintf(stderr, "Too many interfaces (max %d)\n", MAX_INTERFACES);
+        return 1;
+    }
+    for (int i = 1; i < argc; i++) {
+        monitored_ifaces[i-1] = argv[i];
+    }
+
     int sockfd;
     struct sockaddr_nl sa;
     char buf[NL_BUFSIZE];
@@ -69,7 +101,10 @@ int main() {
         return 1;
     }
 
-    printf("Listening for interface create/delete events...\n");
+    printf("Monitoring %d interfaces for UP events:\n", monitored_count);
+    for (int i = 0; i < monitored_count; i++) {
+        printf("  - %s\n", monitored_ifaces[i]);
+    }
 
     while (1) {
         int len = recv(sockfd, buf, sizeof(buf), 0);
